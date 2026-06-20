b48d9afb38fbb888b795b13608b362675b1341e4
Clewo Reshan <137486705+clewoReshan@users.noreply.github.com>
2026-06-20 19:37:31 +0530

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,46 @@
+function openModal(id){
+document.getElementById(id).style.display="block";
+}
+
+function closeModal(id){
+document.getElementById(id).style.display="none";
+}
+
+const topBtn=document.getElementById("topBtn");
+
+window.onscroll=function(){
+
+if(document.body.scrollTop>300 ||
+document.documentElement.scrollTop>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+};
+
+topBtn.addEventListener("click",function(){
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+});
+
+window.onclick=function(event){
+
+const modals=document.querySelectorAll(".modal");
+
+modals.forEach(modal=>{
+
+if(event.target===modal){
+modal.style.display="none";
+}
+
+});
+
+};
